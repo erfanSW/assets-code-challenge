d3e7b52d9360b72b9cec8809bac0644ea83ed4ca
erfanSW <erfanchegini4@gmail.com>
2023-08-12 14:32:15 +0330

    feat: add pages to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,29 @@
-import { RouteRecordRaw, createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
+import AssetsLayout from "@/layouts/AssetsLayout.vue";
+import AssetStatistic from "@/pages/AssetStatistic.vue";
+import AssetsHome from "@/pages/AssetsHome.vue";
 
-const routes: RouteRecordRaw[] = [];
+const routes = [
+  {
+    path: "",
+    redirect: { name: "assets" },
+  },
+  {
+    path: "/assets",
+    name: "assets",
+    component: AssetsLayout,
+    children: [
+      {
+        path: "",
+        component: AssetsHome,
+      },
+      {
+        path: ":id",
+        component: AssetStatistic,
+      },
+    ],
+  },
+];
 const router = createRouter({
   history: createWebHistory(),
   routes,
