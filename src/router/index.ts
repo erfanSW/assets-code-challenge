e32209bbada9d31268eab414bba54f936c0d5bee
erfanSW <erfanchegini4@gmail.com>
2023-08-12 13:28:25 +0330

    init: initialize project

---
+++ src/router/index.ts
@@ -0,0 +1,8 @@
+import { RouteRecordRaw, createRouter, createWebHistory } from "vue-router";
+
+const routes: RouteRecordRaw[] = [];
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+export default router;
